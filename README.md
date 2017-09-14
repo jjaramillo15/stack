e5e2d657ce0b237b4c643ea2933c294e303ebe94
Rhaynel Parra Aguiar <me@rhynl.io>
2017-09-13 23:30:53 -0300

    Add MyStack class

---
+++ README.md
@@ -0,0 +1,15 @@
+# Stack
+> Implementar pilas en Java
+
+Una pila es una estructura de datos, que funciona de tal manera que el ultimo elemento es entrar a la pila es el primer elemento en salir, esto se conoce como LIFO (last in first out).
+
+## Composición de un stack
+
+A una pila se le puede agregar elementos (push) o quitar elementos (pop). De igual manera se puede consultar si la pila esta vacia (isEmpty). Estaremos haciendo uso de [generics](https://docs.oracle.com/javase/tutorial/java/generics/why.html).
+
+## Aplicaciones de un stack
+
+* invertir una palabra.
+* undo/redo en los editores de texto.
+* atras/adelante en los navegadores.
+
---
+++ src/data/sctructure/MyStack.java
@@ -0,0 +1,56 @@
+package data.sctructure;
+
+public class MyStack<T> {
+    private T [] data;
+    private int stackIndex;
+    
+    
+    public MyStack() {
+        data = (T []) new Object[1000];
+        stackIndex = 0;
+    }
+    
+    public void push(T item) {
+        data[stackIndex++] = item;
+    }
+    
+    public T pop() {
+        if(stackIndex == 0) {
+            throw new IllegalStateException("No hay mas elementos en el stack");
+        }
+        
+        return data[--stackIndex];
+    }
+    
+    public boolean contains(T item) {
+        boolean found = false;
+        
+        for (int i = 0; i < stackIndex; i++) {
+            if (data[i].equals(item)) {
+                found = true;
+                break;
+            }
+        }
+        
+        return found;
+    }
+    
+    public T access(T item) {
+        while(stackIndex > 0) {
+            T tmp = pop();
+            if (item.equals(tmp)) {
+                return tmp;
+            }
+        }
+        
+        throw new IllegalArgumentException("No se encontro el item en el stack: " + item);
+    }
+    
+    public int size() {
+        return  stackIndex;
+    }
+    
+    public boolean isEmpty() {
+        return stackIndex == 0;
+    }
+}
